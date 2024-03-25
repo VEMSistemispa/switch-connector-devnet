11a4fdd241b3b83c498ec76815792b755dd35e4c
Giacomo Tontini <giacomo.tontini@vem.com>
2024-03-25 10:36:48 +0100

    fix

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -5,7 +5,4 @@ from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.interface_switchport_configuration_information import InterfaceSwitchportConfigurationInformation
 from swagger_server.models.ipv4_addr import Ipv4Addr
-from swagger_server.models.lldp_neighbors import LldpNeighbors
 from swagger_server.models.mac_address import MACAddress
-from swagger_server.models.mac_address_table import MACAddressTable
-from swagger_server.models.mac_address_table_entry import MACAddressTableEntry
